64ae564a2abf07ee02a3bf6f4e580d23c30b5e52
Leshachok <leshak1207@gmail.com>
2021-10-04 16:32:35 +0300

    added tg library

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,37 +2,26 @@ import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `    
-<div #script style.display="none">
-  <ng-content></ng-content>
-</div>`,
+  templateUrl: "./app.component.html",
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements AfterViewInit {
-  title = 'AIRTable';
+export class AppComponent {
+  title = 'AIRTable'
+  isLoad: boolean = false;
+  isLoadError: boolean = false;
+  userOutput = "";
 
-  @ViewChild('script', { static: true })
-  script!: ElementRef;
+  botName = "ISHIRTestBot";
 
-  convertToScript() {
-    const element = this.script.nativeElement;
-    const script = document.createElement('script');
-    script.src = 'https://telegram.org/js/telegram-widget.js?5';
-    script.setAttribute('data-telegram-login', 'ISHIRTestBot');
-    script.setAttribute('data-size', 'large');
-    // Callback function in global scope
-    script.setAttribute('data-onauth', 'loginViaTelegram(user)');
-    script.setAttribute('data-request-access', 'write');
-    element.parentElement.replaceChild(script, element);
+  onLoad() {
+    this.isLoad = true;
   }
 
-  ngAfterViewInit() {
-    this.convertToScript();
+  onLoadError() {
+    this.isLoadError = true;
   }
 
-  loginViaTelegram(user:any){
-    alert('это хотелось бы чтобы случилось')
+  onLogin(user: any) {
+    this.userOutput = JSON.stringify(user, null, 4);
   }
-
-
 }
