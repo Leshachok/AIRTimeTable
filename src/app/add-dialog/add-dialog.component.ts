7782b8c40e1bee78250a4c58da5461ae1f5307d7
Leshachok <leshak1207@gmail.com>
2021-10-21 12:20:13 +0300

    merge commit

--- src/app/add-dialog/add-dialog.component.ts
+++ src/app/add-dialog/add-dialog.component.ts
@@ -16,13 +16,29 @@ export class AddDialogComponent implements OnInit {
   public pair: boolean = true
   public pair_num: number = 0
   private lecturers: string[] = []
-  public types: string[] = ['lab', 'lecture', 'practice']
+  public types: string[] = []
+  private typesMap: Map<string, string> = new Map([
+    ["Лабораторна", 'lab'],
+    ["Лекція", 'lecture'],
+    ["Практика", 'practice'],
+  ]) 
+  public days: string[] = []
+  private daysMap: Map<string, number> = new Map([
+    ["Понеділок", 1],
+    ["Вівторок", 2],
+    ["Среда", 3],
+    ["Четвер", 4],
+    ["П'ятниця", 5],
+  ])
 
   constructor(private service: TimeTableService) {
     this.group = service.getEditGroup()
+    this.days = [...this.daysMap.keys()]
+    this.types = [...this.typesMap.keys()]
   }
 
   ngOnInit(): void {
+
   }
 
 }
