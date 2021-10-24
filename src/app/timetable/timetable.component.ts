320e277d6e5f6b732589ba2a3efe3b72c7826dfa
Leshachok <leshak1207@gmail.com>
2021-10-24 23:45:39 +0300

    - logs to console + round logo

--- src/app/timetable/timetable.component.ts
+++ src/app/timetable/timetable.component.ts
@@ -67,10 +67,8 @@ export class TimetableComponent implements OnInit, OnChanges {
     ref.onClose.subscribe(
       (pair)=>{
           if(pair){
-            console.log(pair)
             this.service.editPair(this.editAllowGroup, pair).subscribe(
               (response) =>{
-                  console.log(response)
                   this.getPairs()
                   this.messageService.add({severity:'success', summary: 'Змінено', detail: 'Пара успішно змінена!'});
               },
