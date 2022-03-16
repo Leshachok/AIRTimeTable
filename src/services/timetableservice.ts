37a2bd3d14db6bbfcd1a4a01b35943b7213d4c20
Leshachok <leshak1207@gmail.com>
2022-03-16 11:03:45 +0200

    removed logs

--- src/services/timetableservice.ts
+++ src/services/timetableservice.ts
@@ -93,9 +93,7 @@ export class TimeTableService {
         const headers = {
             "authorization": "Bearer " + this.service.getAccessToken()
         }
-        console.log(headers)
-        console.log(body)
-        const url: string = `https://api.timetable.univera.app/lesson/${id}}`;
+        const url: string = `https://api.timetable.univera.app/lesson/${id}`;
         return this.httpClient.post(url, body, { headers: headers })
     }
 
